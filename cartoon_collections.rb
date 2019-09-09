7370ad2c83e6a140dde7655b6df0cc3431ce08ad
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-09 11:54:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(array)
 index = 0 
-
 array.each_with_index {|name, index| puts "#{index + 1}.#{name} "}
 end
 
@@ -15,12 +14,13 @@ def summon_captain_planet(array)
 planeteer_calls  
 end
 
+
+
 def long_planeteer_calls(array)
   
   array.any? do |word| 
   word.length > 4
   end
-
 end
 
 def find_the_cheese(array)
